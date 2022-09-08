985b5b377f67e22d5cd2687b13179090e542c589
Artem2034 <111422217+Artem2034@users.noreply.github.com>
2022-09-08 18:37:52 +0300

    Add files via upload

---
+++ Card.py
@@ -0,0 +1,35 @@
+class Card:
+    suit = ''
+    value = None
+    type = None
+
+class Card:
+
+    def __init__(self, suit, rank, value):
+        self.suit = suit
+        self.rank = rank
+        self.value = value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    suit = [Diamod, Spade, Heart, Club]
+    points = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
